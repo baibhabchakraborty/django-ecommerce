9dcc917874d04204083e7d846727667d0b117878
Baibhab Chakraborty <baibhab.baishakhi@gmail.com>
2021-07-16 11:12:31 +0530

    checkout patchup

--- store/views.py
+++ store/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from .models import *
 
-# Create your views here.
-
-
 def store(request):
     products = Product.objects.all()
     context = {'products':products}
