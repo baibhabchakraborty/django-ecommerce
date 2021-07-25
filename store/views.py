b4575d0fae7228919bab4b3710f41061ba2878f1
Baibhab Chakraborty <baibhab.baishakhi@gmail.com>
2021-07-25 23:47:32 +0530

    update cart quantity

--- store/views.py
+++ store/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-from .models import *
+
 from django.http import JsonResponse
 import json
+from .models import *
 
 def store(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_Cart_total' : 0, 'get_cart_items' : 0}
+        cartItems = order['get_cart_items']
+
+    
     products = Product.objects.all()
-    context = {'products':products}
+    context = {'products':products, 'cartItems':cartItems}
     return render(request, 'store/store.html', context)
 
 def cart(request):
@@ -14,12 +27,13 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer, complete = False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_Cart_total' : 0, 'get_cart_items' : 0}
+        cartItems = order['get_cart_items']
 
-
-    context = {'items' : items, 'order' : order}
+    context = {'items' : items, 'order' : order, 'cartItems':cartItems}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
@@ -27,12 +41,14 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer, complete = False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_Cart_total' : 0, 'get_cart_items' : 0}
+        cartItems = order['get_cart_items']
 
 
-    context = {'items' : items, 'order' : order}
+    context = {'items' : items, 'order' : order, 'cartItems':cartItems}
     return render(request, 'store/checkout.html', context)
 
 
